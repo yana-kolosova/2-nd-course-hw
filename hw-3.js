6e2a2bd1842b61c1060a5c6736e954144aaf51d2
yana-kolosova <brainlesscyborg@gmail.com>
2025-04-25 17:50:24 +0300

    Fixed task 5

--- hw-3.js
+++ hw-3.js
@@ -30,35 +30,21 @@ alert(Number(a) + Number(b));
 let monthNumber = 12;
 switch (monthNumber) {
     case 1:
-        alert("зима");
-        break;
     case 2:
         alert("зима");
         break;
     case 3:
-        alert("весна");
-        break;
     case 4:
-        alert("весна");
-        break;
     case 5:
         alert("весна");
         break;
     case 6:
-        alert("лето");
-        break;
     case 7:
-        alert("лето");
-        break;
     case 8:
         alert("лето");
         break;
     case 9:
-        alert("осень");
-        break;
     case 10:
-        alert("осень");
-        break;
     case 11:
         alert("осень");
         break;
