894180bafc6c8309a96791f6b46870ecd6e9a600
yana-kolosova <brainlesscyborg@gmail.com>
2025-04-30 22:58:25 +0300

    Wrote the js code for game 1

---
+++ script.js
@@ -0,0 +1,23 @@
+function game1() {
+    const randomNumber = Math.ceil(Math.random() * 101);
+    while (true) {
+        let playerInput = Number(prompt("Попробуйте угадать число от 1 до 100:"));
+        if (playerInput === 0) {
+            alert("Удачи в следующий раз!");
+            return;
+        }
+        else if (playerInput === randomNumber) {
+            alert("Ты угадал!");
+            return;
+        }
+        else if (playerInput < randomNumber) {
+            alert(`Не угадал! Подсказка: число должно быть больше. ${randomNumber}`);
+        }
+        else if (playerInput > randomNumber) {
+            alert(`Не угадал! Подсказка: число должно быть меньше. ${randomNumber}`);
+        }
+        else {
+            alert("Пожалуйста, введите число от 1 до 100!")
+        }
+    }
+}
